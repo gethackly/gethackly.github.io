b3e39f39610162c91c907aedc4b6754698e8cd45
Hackly <hellohackly@gmail.com>
2024-10-30 16:47:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ acc[i].addEventListener("click", function() {
 }
 
 //COUNTDOWN
-var countDownDate = new Date("November 27, 2024 00:00:01").getTime();
+var countDownDate = new Date("November 27, 2024 16:00:01").getTime();
 var timeClear = setInterval(function() {
 var now = new Date().getTime();
 var timeLeft = countDownDate - now;

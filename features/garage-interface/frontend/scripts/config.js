241e4136b6cf5eb63bd7e3d170cee2206da6d163
Mugiballa7 <tallballa50@gmail.com>
2025-06-03 16:44:03 +0200

    feat: add Firebase config directly in config.js

---
+++ features/garage-interface/frontend/scripts/config.js
@@ -0,0 +1,10 @@
+// Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyAhqF7t5-3pKVf5a3O8xR60OokawSRuiK0",
+  authDomain: "hackly-mvp.firebaseapp.com",
+  projectId: "hackly-mvp",
+  storageBucket: "hackly-mvp.firebasestorage.app",
+  messagingSenderId: "827934530206",
+  appId: "1:827934530206:web:74e3b56884475c871bf98f",
+  measurementId: "G-J140SVTKRV"
+}; 
